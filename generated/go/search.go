c42d0c5996f13414b466c5cdeedb0285f87b34f7
David Konigsberg <72822263+davidkonigsberg@users.noreply.github.com>
2024-02-16 12:28:22 -0500

    add new endpoint and test

--- generated/go/search.go
+++ generated/go/search.go
@@ -61,6 +61,21 @@ type SearchRecord struct {
 	Advanced *bool `json:"advanced,omitempty"`
 }
 
+type SearchRecordGet struct {
+	// A limit on the number of objects to be returned with a range between 1 and 100. Defaults to 100.
+	Limit *int `json:"-"`
+	// Number of results to skip before returning response. Defaults to 0.
+	Offset *int `json:"-"`
+	// Query term. The syntax for the query parameter follows elasticsearch simple query string syntax. The includes the ability to use search operators and to perform nested queries. Must be url encoded.
+	Q string `json:"-"`
+	// Record or entity fields to search against.
+	Fields []*SearchField `json:"-"`
+	// Whether or not to return search facets in results giving counts by field. Defaults to false.
+	Facets *bool `json:"-"`
+	// Set to true to enable full elasticsearch query string syntax which allows for fielded search and more complex operators. Note that the syntax is more strict and can result in empty result-sets. Defaults to false.
+	Advanced *bool `json:"-"`
+}
+
 // OK
 type EntitySearchResponse struct {
 	Limit  int              `json:"limit"`
--- generated/go/search/client.go
+++ generated/go/search/client.go
@@ -357,3 +357,115 @@ func (c *Client) SearchRecord(
 	}
 	return response, nil
 }
+
+// Search for a record. Please note, searches are limited to a maximum of 10,000 results.
+func (c *Client) SearchRecordGet(
+	ctx context.Context,
+	request *generatedgo.SearchRecordGet,
+	opts ...option.RequestOption,
+) (*generatedgo.RecordSearchResponse, error) {
+	options := core.NewRequestOptions(opts...)
+
+	baseURL := "https://api.sayari.com"
+	if c.baseURL != "" {
+		baseURL = c.baseURL
+	}
+	if options.BaseURL != "" {
+		baseURL = options.BaseURL
+	}
+	endpointURL := baseURL + "/" + "v1/search/record"
+
+	queryParams := make(url.Values)
+	if request.Limit != nil {
+		queryParams.Add("limit", fmt.Sprintf("%v", *request.Limit))
+	}
+	if request.Offset != nil {
+		queryParams.Add("offset", fmt.Sprintf("%v", *request.Offset))
+	}
+	queryParams.Add("q", fmt.Sprintf("%v", request.Q))
+	for _, value := range request.Fields {
+		queryParams.Add("fields", fmt.Sprintf("%v", *value))
+	}
+	if request.Facets != nil {
+		queryParams.Add("facets", fmt.Sprintf("%v", *request.Facets))
+	}
+	if request.Advanced != nil {
+		queryParams.Add("advanced", fmt.Sprintf("%v", *request.Advanced))
+	}
+	if len(queryParams) > 0 {
+		endpointURL += "?" + queryParams.Encode()
+	}
+
+	headers := core.MergeHeaders(c.header.Clone(), options.ToHeader())
+
+	errorDecoder := func(statusCode int, body io.Reader) error {
+		raw, err := io.ReadAll(body)
+		if err != nil {
+			return err
+		}
+		apiError := core.NewAPIError(statusCode, errors.New(string(raw)))
+		decoder := json.NewDecoder(bytes.NewReader(raw))
+		switch statusCode {
+		case 400:
+			value := new(generatedgo.BadRequest)
+			value.APIError = apiError
+			if err := decoder.Decode(value); err != nil {
+				return apiError
+			}
+			return value
+		case 401:
+			value := new(generatedgo.Unauthorized)
+			value.APIError = apiError
+			if err := decoder.Decode(value); err != nil {
+				return apiError
+			}
+			return value
+		case 405:
+			value := new(generatedgo.MethodNotAllowed)
+			value.APIError = apiError
+			if err := decoder.Decode(value); err != nil {
+				return apiError
+			}
+			return value
+		case 406:
+			value := new(generatedgo.NotAcceptable)
+			value.APIError = apiError
+			if err := decoder.Decode(value); err != nil {
+				return apiError
+			}
+			return value
+		case 429:
+			value := new(generatedgo.RateLimitExceeded)
+			value.APIError = apiError
+			if err := decoder.Decode(value); err != nil {
+				return apiError
+			}
+			return value
+		case 500:
+			value := new(generatedgo.InternalServerError)
+			value.APIError = apiError
+			if err := decoder.Decode(value); err != nil {
+				return apiError
+			}
+			return value
+		}
+		return apiError
+	}
+
+	var response *generatedgo.RecordSearchResponse
+	if err := c.caller.Call(
+		ctx,
+		&core.CallParams{
+			URL:          endpointURL,
+			Method:       http.MethodGet,
+			MaxAttempts:  options.MaxAttempts,
+			Headers:      headers,
+			Client:       options.HTTPClient,
+			Response:     &response,
+			ErrorDecoder: errorDecoder,
+		},
+	); err != nil {
+		return nil, err
+	}
+	return response, nil
+}
