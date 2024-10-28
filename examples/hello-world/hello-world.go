07819cdcb6cffaba08545556ba8b35cee5ddf6f8
David Konigsberg <72822263+davidkonigsberg@users.noreply.github.com>
2024-10-28 09:58:07 -0400

    Update hello-world.go

--- examples/hello-world/hello-world.go
+++ examples/hello-world/hello-world.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"github.com/joho/godotenv"
 	"log"
 	"os"
 
@@ -12,6 +13,13 @@ import (
 func main() {
 	// NOTE: To connect you most provide your client ID and client secret. To avoid accidentally checking these into git,
 	// it is recommended to use ENV variables
+	// load ENV file if ENV vars are not set
+	if os.Getenv("CLIENT_ID") == "" || os.Getenv("CLIENT_SECRET") == "" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatalf("Failed to load .env file. Err: %v", err)
+		}
+	}
 
 	// Create a client to auth against the API
 	client, err := sdk.Connect(os.Getenv("CLIENT_ID"), os.Getenv("CLIENT_SECRET"))
